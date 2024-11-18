6d2d9a11ea4422eff369c18d8815eda0d4e9d13f
ImSqueezy <ouaallaabdelali1@gmail.com>
2024-11-18 23:07:07 +0100

    modified printf.h

--- printf.h
+++ printf.h
@@ -4,11 +4,14 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <limits.h>
-# include "libft/libft.h"
+# include "Libft/libft.h"
 # include <stdio.h> // temporarly
 
 void	ft_putchar_count(int *i, char c);
 void	ft_putstr_count(int	*i, char *s);
 void	ft_putnbr_count(int	*i, int nbr);
+void	ft_putu_count(int *i, unsigned int u);
+void	ft_puthex_count(int *i, char c, unsigned int u);
+void	ft_putptr_count(int *i, void *p);
 
 #endif
