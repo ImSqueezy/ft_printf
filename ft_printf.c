012db46ef3ca661da65c457f47caba3af9b076b2
Abdelali Ouaalla <aouaalla@e2r12p2.1337.ma>
2024-11-18 12:04:22 +0100

    modifed ft_printf.c
    handled %s

--- ft_printf.c
+++ ft_printf.c
@@ -1,24 +1,20 @@
 #include "printf.h"
 
-static void	specifier_handle(size_t *count, char c, va_list p2)
+static void	specifier_handle(int *count, char c, va_list p2)
 {
-	char	cha;
-
 	if (c == '\0')
 		return ;
 	else if (c == 'c')
-	{
-		cha = va_arg(p2, int);
-		ft_putchar(cha);
-		*count++;
-	}
+		ft_putchar_count(count, va_arg(p2, int));
+	else if (c == 's')
+		ft_putstr_count(count, va_arg(p2, char *));
 }
 
 
 static int	count_elements(const char *ptr, va_list p)
 {
 	size_t	i;
-	size_t	count;
+	int		count;
 
 	count = 0;
 	i = 0;
@@ -33,10 +29,7 @@ static int	count_elements(const char *ptr, va_list p)
 		{
 			i++;
 			if (ptr[i])
-			{
 				specifier_handle(&count , ptr[i], p);
-				count++;
-			}
 		}
 		i++;
 	}
@@ -45,7 +38,7 @@ static int	count_elements(const char *ptr, va_list p)
 
 int	ft_printf(const char *str, ...)
 {
-	size_t	count;
+	int 	count;
 	va_list p;
 
 	va_start(p, str);
@@ -54,9 +47,11 @@ int	ft_printf(const char *str, ...)
 	return (count);
 }
 
-int main(int ac, char **av)
+#include <stdio.h>
+#include <string.h>
+int main()
 {
-	printf("mine %d", ft_printf("hello %c%c%c%c", 'a', 'b', 'c', 'd'));
-	printf("real %d", printf("hello %c%c%c%c", 'a', 'b', 'c', 'd'));
+	printf("length %zu\n", strlen("hello hello \nworld! a"));
+	ft_printf("hello %s %c\n", "hello world!", 'a');
 	return (0);
 }
