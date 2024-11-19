de891447e381682dff9e849eeacc6cc12515cbbb
ImSqueezy <ouaallaabdelali1@gmail.com>
2024-11-19 12:51:37 +0100

    modified ft_printf.c
    the mandatory part is finished

--- ft_printf.c
+++ ft_printf.c
@@ -2,9 +2,7 @@
 
 static void	specifier_handler(int *count, char c, va_list p2)
 {
-	if (c == '\0')
-		return ;
-	else if (c == 's')
+	if (c == 's')
 		ft_putstr_count(count, va_arg(p2, char *));
 	else if (c == 'c')
 		ft_putchar_count(count, va_arg(p2, int));
@@ -16,10 +14,13 @@ static void	specifier_handler(int *count, char c, va_list p2)
 		ft_putu_count(count, va_arg(p2, unsigned int));
 	else if (c == 'x' || c == 'X')
 		ft_puthex_count(count, c, va_arg(p2, unsigned int));
-	// else if (c == 'p')
-	// 	ft_pu
+	else if (c == 'p')
+	{
+		ft_putchar_count(count, '0');
+		ft_putchar_count(count, 'x');
+		ft_putptr_count(count, va_arg(p2, void *));
+	}
 }
-
 
 static int	count_elements(const char *ptr, va_list p)
 {
@@ -61,7 +62,7 @@ int	ft_printf(const char *str, ...)
 #include <string.h>
 int main()
 {
-	printf("[%d]", printf("hello %s %% %X\n", "hello world!", -2));
-	printf("[%d]", ft_printf("hello %s %% %X\n", "hello world!", -2));
+	printf("[%d]", printf("hello %#x\n", 15));
+	printf("[%d]", ft_printf("hello %x\n", 15));
 	return (0);
 }
