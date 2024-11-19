7ca67d15bebc521614d9f258d958f7f93ccc7060
ImSqueezy <ouaallaabdelali1@gmail.com>
2024-11-19 21:48:33 +0100

    modified ft_putptr_count.c

--- ft_putptr_count.c
+++ ft_putptr_count.c
@@ -6,14 +6,11 @@ void	ft_putptr_count(int *i, void *p)
 	char			*hex_order = "0123456789abcdef";
 
 	address = (unsigned long)p;
-	if (!address)
-		ft_putchar_count(i, '0');
-	else if (address < 16)
-		ft_putchar_count(i, hex_order[address]);
-	else
+	if (address >= 16)
 	{
-		
 		ft_putptr_count(i, (void *)(address / 16));
 		ft_putchar_count(i, hex_order[address % 16]);
 	}
+	else
+		ft_putchar_count(i, hex_order[address]);
 }
